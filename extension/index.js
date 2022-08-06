70e30ee8c466d77f75abe9bb7bc2928f50c4c8a9
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2022-08-06 22:20:41 +0700

    🐛 error on latest code binary

--- extension/index.js
+++ extension/index.js
@@ -112,7 +112,7 @@ function activate(context) {
 
 	var appDir = path.dirname(require.main.filename);
 
-	var HTMLFile = appDir + '/vs/code/electron-browser/workbench/workbench.html';
+	var HTMLFile = appDir + '/vs/code/electron-sandbox/workbench/workbench.html';
 	var JSFile = appDir + '/main.js';
 
 	var runtimeVersion = 'v6';
@@ -238,7 +238,7 @@ function activate(context) {
 
 		try {
 			await fs.stat(JSFile);
-			
+
 			await uninstallJS();
 		} catch (error) {
 			if (error && (error.code === 'EPERM' || error.code === 'EACCES')) {
