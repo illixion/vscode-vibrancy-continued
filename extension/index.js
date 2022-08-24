484ffa526c3f9c52531466ba0fd605f24a8c2a8e
Manual <am@catto.io>
2022-08-24 16:45:02 +0300

    fix disable vibrancy action

--- extension/index.js
+++ extension/index.js
@@ -169,20 +169,19 @@ function activate(context) {
 
 	async function uninstallJS() {
 		const JS = await fs.readFile(JSFile, 'utf-8');
-		const needClean = /\/\* !! VSCODE-VIBRANCY-START !! \*\/[\s\S]*?\/\* !! VSCODE-VIBRANCY-END !! \*\//.test(JS);
+		const needClean = /\n\/\* !! VSCODE-VIBRANCY-START !! \*\/[\s\S]*?\/\* !! VSCODE-VIBRANCY-END !! \*\//.test(JS);
 		if (needClean) {
 			const newJS = JS
-				.replace(/\/\* !! VSCODE-VIBRANCY-START !! \*\/[\s\S]*?\/\* !! VSCODE-VIBRANCY-END !! \*\//, '')
+				.replace(/\n\/\* !! VSCODE-VIBRANCY-START !! \*\/[\s\S]*?\/\* !! VSCODE-VIBRANCY-END !! \*\//, '')
 			await fs.writeFile(JSFile, newJS, 'utf-8');
 		}
 	}
 
 	async function uninstallHTML() {
 		const HTML = await fs.readFile(HTMLFile, 'utf-8');
-		const needClean = /<!-- !! VSCODE-VIBRANCY-START !! -->[\s\S]*?<!-- !! VSCODE-VIBRANCY-END !! -->/.test(HTML);
+		const needClean = / VscodeVibrancy;/.test(HTML);
 		if (needClean) {
-			const newHTML = HTML
-				.replace(/<!-- !! VSCODE-VIBRANCY-START !! -->[\s\S]*?<!-- !! VSCODE-VIBRANCY-END !! -->/, '')
+			const newHTML = HTML.replace(" VscodeVibrancy;", ";").replace(";  trusted-types", "; trusted-types")
 			await fs.writeFile(HTMLFile, newHTML, 'utf-8');
 		}
 	}
