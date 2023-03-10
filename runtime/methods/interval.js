044fba244f31279795f8ab90783852b6296a4b3b
arily <arily@arily.moe>
2023-03-10 15:29:31 +0900

    interval requires config

--- runtime/methods/interval.js
+++ runtime/methods/interval.js
@@ -1,5 +1,25 @@
+/**
+ * @type {{
+ *  os: string,
+ *  config: {
+ *    type:  "auto" | "acrylic" | "appearance-based" | "light" | "dark" | "titlebar" | "selection" | "menu" | "popover" | "sidebar" | "medium-light" | "ultra-dark",
+ *    opacity: number,
+ *    theme: "Default Dark" | "Dark (Only Subbar)" | "Default Light" | "Light (Only Subbar)" | "Tokyo Night Storm" | "Tokyo Night Storm (Outer)" | "Noir et blanc" | "Dark (Exclude Tab Line)" | "Solarized Dark+",
+ *    imports: string[],
+ *    refreshInterval: number,
+ *    preventFlash: boolean
+ *  },
+ *  themeCSS: string,
+ *  theme: any,
+ *  imports: {
+ *    css: string,
+ *    js: string
+ *  }
+ * }}
+ */
+const app = global.vscode_vibrancy_plugin;
 let backgroundColorTimer;
-module.exports = window => ({
+module.exports = (window) => ({
 	install() {
 		clearInterval(backgroundColorTimer);
 		// https://github.com/microsoft/vscode/blob/9f8431f7fccf7a048531043eb6b6d24819482781/src/vs/platform/theme/electron-main/themeMainService.ts#L80
