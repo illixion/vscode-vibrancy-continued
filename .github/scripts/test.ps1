12f2b318fade76f63cce7469c25068eaae15e488
Ixion <illixion@illixion.com>
2025-07-30 19:03:21 +0300

    Update UI test

---
+++ .github/scripts/test.ps1
@@ -0,0 +1,12 @@
+# Print a list of all currently running processes
+Get-Process | ForEach-Object {
+    Write-Host "Process: $($_.ProcessName) (ID: $($_.Id))"
+}
+
+# Also print a list of all processes that currently have a window open
+Get-Process | Where-Object { $_.MainWindowHandle -ne 0 } | ForEach-Object {
+    Write-Host "Process with window: $($_.ProcessName) (ID: $($_.Id))"
+}
+
+# Early exit for test
+exit 1
