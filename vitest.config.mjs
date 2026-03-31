93669668241ba2b566e38a4634f1edb5d3e9184d
Ixion <illixion@illixion.com>
2026-03-31 11:14:47 +0300

    Update test report parser, add status check

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,7 +4,10 @@ export default defineConfig({
   test: {
     globals: true,
     reporters: process.env.GITHUB_ACTIONS
-      ? ['default', 'github-actions']
+      ? ['default', 'junit']
       : ['default'],
+    outputFile: process.env.GITHUB_ACTIONS
+      ? { junit: 'test-results/vitest-report.xml' }
+      : undefined,
   },
 });
